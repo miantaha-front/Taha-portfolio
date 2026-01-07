1187e72bae00784de128152530dcc2ea94720feb
miantaha-front <miantaha098@gmail.com>
2026-01-07 23:32:32 +0500

    Fix homepage link

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import logo from './logo.svg';
 import './App.css';
-import Navbar from './Components/navbar';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import NavigationBar from './Components/navbar';
 import SectionA from './Components/SectionA';
